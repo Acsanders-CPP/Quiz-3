282b931e7b9d9ee77c467b9fd070dec8665bfb4b
unknown <sanders.aidan.c.ss@gmail.com>
2022-10-23 19:31:15 -0700

    Initial commit

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+  // make menu
+  printf("--------------------\nGuessing Game\n1.) ");
+
+  // write functions for guessing
+
+  //return to main menu
+  return 0;
+}
