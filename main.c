5b8f4acdf1bb7657515698ae761c794fc8e0507f
unknown <sanders.aidan.c.ss@gmail.com>
2022-10-23 21:36:08 -0700

    inital commit with comments

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void TestSquare(int square[][]);
+
+int main(void) {
+  printf("Hello World\n");
+  return 0;
+}
+//test square function
+void TestSquare
+
+//random square function
