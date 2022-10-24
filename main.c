4e58da39ce2cb9b55b03039f48168919a2f381b4
unknown <sanders.aidan.c.ss@gmail.com>
2022-10-23 20:43:17 -0700

    Initial commit copying code

---
+++ main.c
@@ -0,0 +1,98 @@
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+int ChangeMaxNumber();
+void MainMenu();
+void PlayMatch(int maxNumber);
+
+int main(void) {
+  srand(time(0));
+  
+  MainMenu();
+  
+  return 0;
+}
+
+int ChangeMaxNumber(){
+  int input = -1;
+  
+  while ((input < 1) || (input > INT_MAX)){
+    printf("Enter the max number:\n");
+
+    scanf("%d", &input);
+  }
+
+  printf("Current max is now %d\n", input);
+
+  return input;
+}
+
+void MainMenu(){
+  int input = 0;
+  int maxNumber = 10;
+
+
+  while (1){
+    input = 0;
+    
+    // make menu
+    printf("-------------------------\nGuessing Game\n\n1.) Play Game\n2.) Change Max Number\n3.) Exit\n-------------------------\n");
+
+    while ((input > 3) || (input < 1)){
+      printf("Enter a number:\n");
+
+      scanf("%d", &input);
+    }
+
+    switch (input){
+      case 1:
+        PlayMatch(maxNumber);
+        break;
+      case 2:
+        maxNumber = ChangeMaxNumber();
+        break;
+      case 3:
+        return;
+      default:
+        printf("Invalid input\n");
+        break;
+    }
+  }
+}
+
+// write functions for guessing
+void PlayMatch(int maxNumber){
+  char converter;
+  int input;
+  int correctNumber = (rand() % maxNumber) + 1;
+
+  while (1){
+    printf("Guess a number (Current max is %d):\n", maxNumber);
+
+    scanf("%s", &converter);
+
+    if (strcmp(&converter, "q") == 0){
+      printf("The correct answer was %d\n", correctNumber);
+
+      return;
+    }
+
+    input = atoi(&converter);
+    
+    if (input > correctNumber){
+      printf("Lower\n");
+    }
+    else if (input < correctNumber){
+      printf("Higher\n");
+    }
+    else{
+      printf("You win\n");
+
+      return;
+    }
+  }
+}
+//return to main menu
