c363abc4fd2bc8fc3ed898fc38c3b86b89efd133
unknown <sanders.aidan.c.ss@gmail.com>
2022-10-23 19:43:38 -0700

    chnaged menu to be its own function and began gameplay implementation

--- main.c
+++ main.c
@@ -1,11 +1,40 @@
 #include <stdio.h>
 
+void MainMenu();
+void PlayMatch();
+
 int main(void) {
+  MainMenu();
+  
+  return 0;
+}
+
+void MainMenu(){
+  int input = 0;
+  
   // make menu
   printf("-------------------------\nGuessing Game\n\n1.) Play Game\n2.) Change Max Number\n3.) Exit\n-------------------------\n");
 
-  // write functions for guessing
+  while ((input > 3) && (input < 1)){
+    printf("Enter a number:\n");
 
-  //return to main menu
-  return 0;
+    scanf("%d", &input);
+  }
+
+  switch (input){
+    case 1:
+      PlayMatch();
+  }
 }
+
+// write functions for guessing
+void PlayMatch(){
+  int input;
+  
+  printf("Guess a number:\n");
+
+  scanf("%d", &input);
+
+  if (input ==)
+}
+//return to main menu
