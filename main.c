98a86a33fbf1f7de22458d8e0fb7deee6147cf14
unknown <sanders.aidan.c.ss@gmail.com>
2022-10-23 19:34:53 -0700

    edited menu options and added detail

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 
 int main(void) {
   // make menu
-  printf("--------------------\nGuessing Game\n1.) ");
+  printf("-------------------------\nGuessing Game\n\n1.) Play Game\n2.) Change Max Number\n3.) Exit\n-------------------------\n");
 
   // write functions for guessing
 
